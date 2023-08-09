6886880f6db64ed97509ee4c512e3ec21e8ac124
mrthnby <meralihanbay@gmail.com>
2023-08-09 19:48:50 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Mert Ali Hanbay on 9.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
